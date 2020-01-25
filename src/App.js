04037cf245ce0953d37f35391f83347401c2d0a5
unknown <daheimao003@gmail.com>
2020-01-24 21:26:58 -0600

    Create Veiw

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React ,{Fragment}from 'react';
 import './App.css';
+import Header from './header'
+import Movie from './movie'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Fragment>
+      <Header/>
+      <Movie/>
+      <Movie/>
+      <Movie/>
+      <Movie/>
+    </Fragment>
   );
 }
 
---
+++ src/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header (){
+ return(
+  <header id="header">
+    <h1 id="headText">Popular Metal Albums</h1>
+  </header>
+ )
+}
+
+export default Header;
